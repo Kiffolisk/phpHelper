defac195e413a774d4f9d806147ee9644519e845
GAMERSUNITE <45130240+Kiffolisk@users.noreply.github.com>
2020-11-30 18:09:01 +0100

    Create GetPost.php

---
+++ GetPost.php
@@ -0,0 +1,9 @@
+<?php
+	function GetPost($typeofthing, $thingto){
+		if ($typeofthing == 'GET'){
+			return $_GET[$thingto]
+		}elseif ($typeofthing == 'POST'){
+			return $_POST[$thingto]
+		}
+	}
+?>
